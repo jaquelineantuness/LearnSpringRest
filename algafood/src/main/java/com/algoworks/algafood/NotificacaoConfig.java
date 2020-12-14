9cbbcb095e1181b604fe24819e92003d7af44ef0
Jaqueline Cristina Antunes de Souza <antunes.jaqueline186@gmail.com>
2020-12-14 09:09:41 -0300

    Outra forma de definir o Bean, deixando de usar o AlgaConfig, criando classes de configuraçaõ como NotificacaoConfig e ServiceConfig, pode até apagar a AlgaConfig que irá continuar a funcionar o Bean

--- algafood/src/main/java/com/algoworks/algafood/NotificacaoConfig.java
+++ algafood/src/main/java/com/algoworks/algafood/NotificacaoConfig.java
@@ -1,23 +1,16 @@
 package com.algoworks.algafood;
 
 import com.algoworks.algafood.notificacao.NotificacaoEmail;
-import com.algoworks.algafood.service.AtivacaoClienteService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-public class AlgaConfig {
-
+@Configuration
+public class NotificacaoConfig {
     @Bean
     public NotificacaoEmail notificacaoEmail(){
         NotificacaoEmail notificador = new NotificacaoEmail("smtp.algamail.com.br");
         notificador.setCaixaAlta(true);
 
         return notificador;
-    }
-
-    @Bean
-    public AtivacaoClienteService ativacaoClienteService(){
-        return  new AtivacaoClienteService(notificacaoEmail());//já tras um bean gerenciado pelo spring quando chamo notificacaoEmail()
     }
 }
