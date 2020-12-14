4ca568935297ed9c1e20a431fbc58ff5c4866a00
Jaqueline Cristina Antunes de Souza <antunes.jaqueline186@gmail.com>
2020-12-14 08:57:39 -0300

    Definição do Bin NotificacaoEmail, dentro de uma classe de configuracao

---
+++ algafood/src/main/java/com/algoworks/algafood/AlgaConfig.java
@@ -0,0 +1,18 @@
+package com.algoworks.algafood;
+
+import com.algoworks.algafood.notificacao.NotificacaoEmail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AlgaConfig {
+
+    @Bean
+    public NotificacaoEmail notificacaoEmail(){
+        NotificacaoEmail notificador = new NotificacaoEmail("smtp.algamail.com.br");
+        notificador.setCaixaAlta(true);
+
+        return notificador;
+    }
+
+}
--- algafood/src/main/java/com/algoworks/algafood/notificacao/NotificacaoEmail.java
+++ algafood/src/main/java/com/algoworks/algafood/notificacao/NotificacaoEmail.java
@@ -4,20 +4,30 @@ package com.algoworks.algafood.notificacao;
 import com.algoworks.algafood.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
-//@Component inicia o Bin, vai aparecer ao subir a aplicação
-@Component
+
 public class NotificacaoEmail implements Notificador {
 
+    private boolean caixaAlta;
+    private String hostServidorSmtp;
 
-    public NotificacaoEmail(){
+    public NotificacaoEmail(String hostServidorSmtp){
+        this.hostServidorSmtp = hostServidorSmtp;
         System.out.println("NotificadorEmail chamado");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        System.out.printf("Noficando %s através do email %s: %s\n"
+        if(this.caixaAlta){
+            mensagem = mensagem.toUpperCase();
+        }
+        System.out.printf("Noficando %s através do email %s usando SMTP %s: %s\n"
                 , cliente.getNome()
                 , cliente.getEmail()
+                ,this.hostServidorSmtp
                 , mensagem);
+    }
+
+    public void setCaixaAlta(boolean caixaAlta) {
+        this.caixaAlta = caixaAlta;
     }
 }
