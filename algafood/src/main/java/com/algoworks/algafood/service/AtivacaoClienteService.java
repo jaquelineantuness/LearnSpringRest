59fb85d3ab639df0882838c9750a48d8f64c2b01
Jaqueline Cristina Antunes de Souza <antunes.jaqueline186@gmail.com>
2020-12-16 08:52:26 -0300

    Limpando o codigo para implementar a dependencia opcional com Autowide

--- algafood/src/main/java/com/algoworks/algafood/service/AtivacaoClienteService.java
+++ algafood/src/main/java/com/algoworks/algafood/service/AtivacaoClienteService.java
@@ -11,23 +11,11 @@ public class AtivacaoClienteService {
 
     @Autowired
     private Notificador notificador;
-
-    //@Autowired para demonstrar qual contrutor o Spring irá usar ao instanciar.(quando existe apenas 1 construtor não é necessário)
-//    @Autowired
-//    public AtivacaoClienteService(Notificador notificador) {
-//        this.notificador = notificador;
-//
-//    }
-
+    
     public void ativar(Cliente cliente){
         cliente.ativar();
 
         notificador.notificar(cliente,"Seu cadastro no sistema está ativo");
     }
 
-    //@Autowired é o ponto de injeção
-//    @Autowired
-//    public void setNotificador(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
 }
