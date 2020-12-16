81220a83da95bcf17595ebeedcff833f5248af03
Jaqueline Cristina Antunes de Souza <antunes.jaqueline186@gmail.com>
2020-12-16 08:12:58 -0300

    Removendo classes de configuraçaõ para o inicio da aula sobre pontos de enjeção

--- algafood/src/main/java/com/algoworks/algafood/service/AtivacaoClienteService.java
+++ algafood/src/main/java/com/algoworks/algafood/service/AtivacaoClienteService.java
@@ -3,19 +3,28 @@ package com.algoworks.algafood.service;
 
 import com.algoworks.algafood.modelo.Cliente;
 import com.algoworks.algafood.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-
-    }
+    //@Autowired para demonstrar qual contrutor o Spring irá usar ao instanciar.(quando existe apenas 1 construtor não é necessário)
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//
+//    }
 
     public void ativar(Cliente cliente){
         cliente.ativar();
 
         notificador.notificar(cliente,"Seu cadastro no sistema está ativo");
+    }
+
+    public void setNotificador(Notificador notificador) {
+        this.notificador = notificador;
     }
 }
