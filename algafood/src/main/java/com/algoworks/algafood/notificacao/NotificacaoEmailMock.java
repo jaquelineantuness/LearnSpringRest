7299550fc4fd8733da0da9ecfb83ae01bb39fa3f
Jaqueline Cristina Antunes de Souza <antunes.jaqueline186@gmail.com>
2020-12-28 20:50:19 -0300

    Aula 2.20: mudando o comportamento da aplicação com Spring profile

--- algafood/src/main/java/com/algoworks/algafood/notificacao/NotificacaoEmailMock.java
+++ algafood/src/main/java/com/algoworks/algafood/notificacao/NotificacaoEmailMock.java
@@ -2,23 +2,22 @@ package com.algoworks.algafood.notificacao;
 
 
 import com.algoworks.algafood.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
-public class NotificacaoEmail implements Notificador {
+public class NotificacaoEmailMock implements Notificador {
 
-    public NotificacaoEmail(){
-        System.out.println("NotificadorEmail Real");
+    public NotificacaoEmailMock(){
+        System.out.println("NotificadorEmail MoCK");
     }
+
     @Override
     public void notificar(Cliente cliente, String mensagem){
 
-        System.out.printf("Noficando %s através do email %s : %s\n"
+        System.out.printf("MOCK:Noficando seria enviada para %s através do email %s : %s\n"
                 , cliente.getNome()
                 , cliente.getEmail()
                 , mensagem);
