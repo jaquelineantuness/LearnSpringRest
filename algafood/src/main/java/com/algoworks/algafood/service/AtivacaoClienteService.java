bfcc96a723dadc7973a0ebd5083a06af3c69eb34
Jaqueline Cristina Antunes de Souza <antunes.jaqueline186@gmail.com>
2020-12-28 21:01:09 -0300

    Aula 2.21: primeira forma de criar metodo de callback

--- algafood/src/main/java/com/algoworks/algafood/service/AtivacaoClienteService.java
+++ algafood/src/main/java/com/algoworks/algafood/service/AtivacaoClienteService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class AtivacaoClienteService {
 
@@ -16,12 +19,19 @@ public class AtivacaoClienteService {
     @Autowired
     private Notificador notificador;
 
+    @PostConstruct
+    public void unit(){
+        System.out.println("INIT "+ notificador);
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("destroy");
+    }
+
     public void ativar(Cliente cliente){
         cliente.ativar();
 
-
         notificador.notificar(cliente,"Seu cadastro no sistema está ativo");
-
 
     }
 
