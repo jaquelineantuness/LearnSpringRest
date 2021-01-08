6f623a1eb51d4e2d2e6035192b03cd38a871030d
Jaqueline Cristina Antunes de Souza <antunes.jaqueline186@gmail.com>
2021-01-08 16:52:28 -0300

    2.25- Criando e acessando propriedades customizadas, pelo appliocation.properties e add @Value()

--- algafood/src/main/java/com/algoworks/algafood/notificacao/NotificacaoEmail.java
+++ algafood/src/main/java/com/algoworks/algafood/notificacao/NotificacaoEmail.java
@@ -2,7 +2,9 @@ package com.algoworks.algafood.notificacao;
 
 
 import com.algoworks.algafood.modelo.Cliente;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,17 @@ import org.springframework.stereotype.Component;
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificacaoEmail implements Notificador {
+    //pode colocar qualquer nome para usar a propriedade custom
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
+        System.out.println("Host: "+host);
+        System.out.println("Porta: " + porta);
 
         System.out.printf("Noficando %s através do email %s : %s\n"
                 , cliente.getNome()
